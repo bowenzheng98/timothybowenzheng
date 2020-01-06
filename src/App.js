682d26c72c1fb9e97d7516de26736904399279c6
Bowen Zheng <bowenzheng998@gmail.com>
2020-01-06 20:30:35 +1300

    Added temporary page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-        timothybowenzheng
+    <div className="Home-parent">
+        <div className="Home-child">
+            Work in Progress
+        </div>
     </div>
   );
 }
