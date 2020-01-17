d2b0c873d7c44c3d52b82be88018db28aab3a6fd
Bowen Zheng <bowenzheng998@gmail.com>
2020-01-17 23:22:01 +1300

    Added random movementfrom either of the sides

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,49 +13,91 @@ function Home() {
   const [moveDown, setMoveDown] = useState(false);
   const [moveUp, setMoveUp] = useState(false);
 
-
-
   const [pos1, setPos1] = useState(home.centerImageContainer);
   const [pos2, setPos2] = useState(home.leftImageContainer);
-  
+
   const [pos1Visible, setPos1Visible] = useState(true);
 
   function handleClick() {
     setAnimated(true);
+    console.log("Moving " + nextMove);
+    switch (nextMove) {
+      case 0:
+        setMoveRight(true);
+        break;
+      case 1:
+        setMoveLeft(true);
+        break;
+      case 2:
+        setMoveUp(true);
+        break;
+      case 3:
+        setMoveDown(true);
+        break;
+      default:
+        break;
+    }
     setTimeout(() => {
-      if (pos1Visible){
+      var randomnumber = Math.floor(Math.random() * (3 - 0 + 1)) + 0;
+      console.log("Random number is :" + randomnumber);
+      setNextMove(randomnumber);
+      console.log("nextMove is: " + nextMove);
+      if (pos1Visible) {
         setPos2(home.centerImageContainer);
-        switch (nextMove) {
+        switch (randomnumber) {
           case 0:
             setPos1(home.leftImageContainer);
+            break;
+          case 1:
+            setPos1(home.rightImageContainer);
+            break;
+          case 2:
+            setPos1(home.bottomImageContainer);
+            break;
+          case 3:
+            setPos1(home.topImageContainer);
+            break;
+          default:
+            break;
         }
       } else {
         setPos1(home.centerImageContainer);
-        switch (nextMove) {
+        switch (randomnumber) {
           case 0:
             setPos2(home.leftImageContainer);
+            break;
+          case 1:
+            setPos2(home.rightImageContainer);
+            break;
+          case 2:
+            setPos2(home.bottomImageContainer);
+            break;
+          case 3:
+            setPos2(home.topImageContainer);
+            break;
+          default:
+            break;
         }
       }
-      // setPos1(home.leftImageContainer);
-      // setPos2(home.centerImageContainer);
       setAnimated(false);
+      setMoveRight(false);
+      setMoveLeft(false);
+      setMoveUp(false);
+      setMoveDown(false);
       setPos1Visible(!pos1Visible);
-    }, 1000);
-  }
-
-  function determineNextMove(){
-    //0 move right, 1 move left, 2 move up, 3 move down
-    //currently only move right
-    setNextMove(0);
+    }, 1500);
   }
 
   return (
     <div className="page-layout">
       <div
         className={clsx(
-          pos1, 
+          pos1,
           animated ? home.animated : null,
-          animated ? home.moveRight : null,
+          moveRight ? home.moveRight : null,
+          moveLeft ? home.moveLeft : null,
+          moveUp ? home.moveUp : null,
+          moveDown ? home.moveDown : null,
         )}
       >
         <img src={image} onClick={handleClick} alt=""></img>
@@ -63,8 +105,11 @@ function Home() {
       <div
         className={clsx(
           pos2,
-          animated? home.animated : null,
-          animated ? home.moveRight : null,
+          animated ? home.animated : null,
+          moveRight ? home.moveRight : null,
+          moveLeft ? home.moveLeft : null,
+          moveUp ? home.moveUp : null,
+          moveDown ? home.moveDown : null,
         )}
       >
         <img src={art} onClick={handleClick} alt=""></img>
