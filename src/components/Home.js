c18fb3afca4f709cd68b8558dac69e63cb3ed7dd
Bowen Zheng <bowenzheng998@gmail.com>
2020-01-17 22:59:47 +1300

    Created infinite move right loop

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,17 +5,48 @@ import art from "./art-board.png";
 import clsx from "clsx";
 
 function Home() {
+  const [nextMove, setNextMove] = useState(0);
   const [animated, setAnimated] = useState(false);
+
+  const [moveRight, setMoveRight] = useState(false);
+  const [moveLeft, setMoveLeft] = useState(false);
+  const [moveDown, setMoveDown] = useState(false);
+  const [moveUp, setMoveUp] = useState(false);
+
+
+
   const [pos1, setPos1] = useState(home.centerImageContainer);
   const [pos2, setPos2] = useState(home.leftImageContainer);
+  
+  const [pos1Visible, setPos1Visible] = useState(true);
 
   function handleClick() {
-    setAnimated(!animated);
+    setAnimated(true);
     setTimeout(() => {
-      setPos1(home.leftImageContainer);
-      setPos2(home.centerImageContainer);
+      if (pos1Visible){
+        setPos2(home.centerImageContainer);
+        switch (nextMove) {
+          case 0:
+            setPos1(home.leftImageContainer);
+        }
+      } else {
+        setPos1(home.centerImageContainer);
+        switch (nextMove) {
+          case 0:
+            setPos2(home.leftImageContainer);
+        }
+      }
+      // setPos1(home.leftImageContainer);
+      // setPos2(home.centerImageContainer);
       setAnimated(false);
+      setPos1Visible(!pos1Visible);
     }, 1000);
+  }
+
+  function determineNextMove(){
+    //0 move right, 1 move left, 2 move up, 3 move down
+    //currently only move right
+    setNextMove(0);
   }
 
   return (
