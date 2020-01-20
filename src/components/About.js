2efcf5e038a14606d4f7d8f4df947304928a80f4
Bowen Zheng <bowenzheng998@gmail.com>
2020-01-20 17:55:52 +1300

    Removed aboutme string

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,6 @@ function About() {
     return(
         <div className="pagelayout">
             <div className="pageChildCenter">
-                About me
                 <img src={art}></img>
             </div>
         </div>
